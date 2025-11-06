fef6f3d20030a7c7a8cba5f3a13f3cb60986800d
Samson Nguyen <samson@quantifi.ca>
2025-11-05 23:40:04 -0700

    Finalize update script

--- images/inotify-sftp/send.sh
+++ images/inotify-sftp/send.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 timestamp="$1"; shift
-file="$1"; shift
+target="$1"; shift
 event="$1"; shift
+file="$1"; shift
 
-echo "[${timestamp}] file: ${file} changed (${event}), sending"
-scp -i ${IDENTITY_FILE} -s "${file}" ${DESTINATION}
+echo "[${timestamp}] file: ${target}${file} changed (${event}), checking if file exists"
+if [ -f "${target}${file}" ]; then
+  echo "[${timestamp}] file: ${file} changed (${event}), sending"
+  scp -i ${IDENTITY_FILE} "${target}/${file}" ${DESTINATION}
+fi
+
